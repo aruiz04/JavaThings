f39947bfcb24eea5a97225dbe612a008335534a6
Armando Ruiz <armando.ruiz81@hotmail.com>
2022-07-31 20:11:47 -0700

    Added input from keyboard to new String and displays

--- src/Restaurant/App.java
+++ src/Restaurant/App.java
@@ -1,15 +1,30 @@
 package Restaurant;
 
+import java.io.InputStream;
 import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
         ArrayList<SecurityLevel> levels = DAL.getSecurityLevels();
         ArrayList<Position> positions = DAL.getPositions();
-        for (Position p: positions ) {
+
+        for (Position p : positions) {
             System.out.println(p.toString());
         }
-        for(SecurityLevel s : levels){
+        for (SecurityLevel s : levels) {
             System.out.println(s.toString());
         }
+        char inputManager = 'a';
+        System.out.println("Enter something man:");
+        byte[] input = new byte[255];
+
+        try {
+            int i = System.in.read(input, 0, input.length);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        String result = new String(input);
+        System.out.println(result.trim());
+
     }
 }
